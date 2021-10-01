84b9f5ab537a8880505297ccf2e53cbe0ca98f1e
douzone-bipa <douzone.bipa@gmail.com>
2021-10-01 16:18:11 +0900

    조인 연습

--- sql-practices/sql연습/집계연습.sql
+++ sql-practices/sql연습/집계연습.sql
@@ -36,3 +36,11 @@ select avg(salary)
  group by emp_no
    having avg(salary) > 60000
 order by  avg(salary) asc;  
+
+-- 예제
+-- salaries 테이블에서 사번이 10060인 직원의 급여 평균과 총합을 출력하세요.
+-- 문법적으로 오류
+-- 의미적으로 맞다(where)
+select emp_no, avg(salary), sum(salary)
+  from salaries  
+ where emp_no='10060';
