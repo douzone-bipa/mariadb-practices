98b1af466eaaabf375c6f028bae2a435a71d4b57
douzone-bipa <douzone.bipa@gmail.com>
2021-10-01 18:00:14 +0900

    오류수정

--- sql-practices/sql연습/문자열함수.sql
+++ sql-practices/sql연습/문자열함수.sql
@@ -13,7 +13,7 @@ select substring('Hello World', 3, 2);
 -- 예제) 1989년에 입사한 사원들의 이름, 입사일 출력
 select first_name, hire_date
   from employees
- where hire_date = substring(hire_date, 1, 4);
+ where '1989' = substring(hire_date, 1, 4);
 
 -- lpad(오른쪽 정렬), rpad(왼쪽 정렬)
 select lpad('1234', 10, '-');
